a52038e15a1c726134198caa3c2a2062986378a9
ekirken1 <kirkendall.eve@gmail.com>
2019-11-23 09:17:40 -0500

    ex 27-30.

---
+++ ex27_ex28_memorizing_logic.py
@@ -0,0 +1,20 @@
+True and True
+False and True
+1 == 1 and 2 == 1
+"test" == "test"
+1 == 1 or 2 != 1
+True and 1 == 1
+False and 0 != 0
+True or 1 == 1
+"test" == 'testing'
+1 != 0 and 2 ==1
+"test" != "testing" #TT
+"test" == 1 # FF
+not (True and False)
+not (1 == 1 and 0 != 1) #FF
+not (10 == 1 or 1000 == 1000) #FF
+not (1 != 10 or 3 == 4) #FF
+not ("testing" == "testing" and "Zed" == "Cool guy") #TT
+1 == 1 and (not ("testing" == 1 or 1 == 0)) #TT
+"chunky" == "bacon" and (not (3 == 4 or 3 ==3)) #FF
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")) #FF
